957ad6c0b2b4028a7ff1641b69b299f333a326f0
Ibai <iibaiizj@gmail.com>
2025-05-15 01:04:18 +0200

    Refactor configuración de seguridad: agregar manejador de éxito de inicio de sesión, servicio de detalles de usuario y mejorar la gestión de usuarios; eliminar LoginController y ajustar UserController.

---
+++ src/main/java/com/athletix/controller/AccountController.java
@@ -0,0 +1,55 @@
+package com.athletix.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.athletix.model.User;
+import com.athletix.service.UserService;
+
+@Controller
+public class AccountController {
+    private static final Logger log = LoggerFactory.getLogger(PublicController.class);
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/login")
+    public String login() {
+        /*
+         * Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+         * 
+         * if (auth != null && auth.isAuthenticated() &&
+         * !auth.getPrincipal().equals("anonymousUser")) {
+         * return "pages/about";
+         * }
+         */
+        log.info("Login page accessed");
+        return "pages/login";
+    }
+
+    @GetMapping("/create-account")
+    public String showCreateAccountForm() {
+        log.info("Create Account page accessed");
+        return "pages/createAccount";
+    }
+
+    @PostMapping("/create-account")
+    public String createAccount(@ModelAttribute("user") User user, @RequestParam(required = false) boolean valid) {
+        try {
+            userService.registerUser(user.getUsername(), user.getPassword());
+            log.info("Account registed for user: {}", user.getUsername());
+            return "redirect:/login?registered";
+        } catch (IllegalArgumentException e) {
+            // model.addAttribute("error", e.getMessage());
+            log.error("Error creating account: {}", e.getMessage());
+            return "redirect:/create-account?error=" + e.getMessage();
+        }
+    }
+
+}
--- src/main/java/com/athletix/model/User.java
+++ src/main/java/com/athletix/model/User.java
@@ -1,19 +1,29 @@
 package com.athletix.model;
 
+import java.io.Serializable;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity
 @Getter
 @Setter
-public class User {
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-    private String name;
-    private String email;
+    private String username;
+    private String password;
+    // private String name;
+    // private String surname;
+    // private String phone;
+    // private String email;
+    private String role;
 }
