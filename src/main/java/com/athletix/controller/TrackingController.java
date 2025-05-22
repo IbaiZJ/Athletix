5f1e39d5f1d96de8ff2e5a14ff6fd478ecb1b201
Ibai <iibaiizj@gmail.com>
2025-05-22 15:25:40 +0200

    feat: Agregar controlador de seguimiento y archivo de captura de pantalla

--- src/main/java/com/athletix/controller/TrackingController.java
+++ src/main/java/com/athletix/controller/TrackingController.java
@@ -2,10 +2,12 @@ package com.athletix.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/tracking")
+@Controller
 public class TrackingController {
     private static final Logger log = LoggerFactory.getLogger(TrackingController.class);
 
