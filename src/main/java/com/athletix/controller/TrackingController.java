9921affdf7adb4506f3937560eb60264f97a4706
Ibai <iibaiizj@gmail.com>
2025-05-22 15:16:19 +0200

    feat: Agregar página de seguimiento y mejorar el controlador de seguimiento

--- src/main/java/com/athletix/controller/TrackingController.java
+++ src/main/java/com/athletix/controller/TrackingController.java
@@ -1,13 +1,17 @@
 package com.athletix.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/tracking")
 public class TrackingController {
+    private static final Logger log = LoggerFactory.getLogger(TrackingController.class);
 
     @GetMapping("")
-    public String showMyTracking() {
+    public String showMyTrackings() {
+        log.info("Tracking page accessed");
         return "pages/tracking";
     }
 
