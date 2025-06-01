f439baa31f63dc8c3e432ee57244ae465a01646f
Ibai <iibaiizj@gmail.com>
2025-06-01 21:38:32 +0200

    notification backend

---
+++ src/main/java/com/athletix/model/DTO/notificationsDeleteDTO/NotificationDeleteRequest.java
@@ -0,0 +1,10 @@
+package com.athletix.model.DTO.notificationsDeleteDTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NotificationDeleteRequest {
+    private Integer notificationId;
+}
