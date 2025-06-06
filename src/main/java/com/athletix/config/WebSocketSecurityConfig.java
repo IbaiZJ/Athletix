33ec7bacfb5a1810d992f095cbabb23ef19d6f8c
Ibai <iibaiizj@gmail.com>
2025-06-07 00:54:01 +0200

    web socket conf

---
+++ src/main/java/com/athletix/config/WebSocketSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.athletix.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+
+@Configuration
+public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+
+    // @Override
+    // protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+    //     messages
+    //             .simpDestMatchers("/app/**").authenticated()
+    //             .simpSubscribeDestMatchers("/user/queue/**").authenticated()
+    //             .anyMessage().denyAll();
+    // }
+
+    // @Override
+    // protected boolean sameOriginDisabled() {
+    //     return true; // Solo para desarrollo, en producción configurar CORS adecuadamente
+    // }
+}
