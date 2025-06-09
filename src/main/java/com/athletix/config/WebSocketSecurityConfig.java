3bfea4fc896aefc4e4cf306862e34fcb967d2f5e
Ibai <iibaiizj@gmail.com>
2025-06-09 14:53:41 +0200

    remove ws secur

--- src/main/java/com/athletix/config/WebSocketSecurityConfig.java
+++ src/main/java/com/athletix/config/WebSocketSecurityConfig.java
@@ -3,19 +3,19 @@ package com.athletix.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 
-@Configuration
-public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+// @Configuration
+// public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
 
-    // @Override
-    // protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
-    //     messages
-    //             .simpDestMatchers("/app/**").authenticated()
-    //             .simpSubscribeDestMatchers("/user/queue/**").authenticated()
-    //             .anyMessage().denyAll();
-    // }
+//     // @Override
+//     // protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+//     //     messages
+//     //             .simpDestMatchers("/app/**").authenticated()
+//     //             .simpSubscribeDestMatchers("/user/queue/**").authenticated()
+//     //             .anyMessage().denyAll();
+//     // }
 
-    // @Override
-    // protected boolean sameOriginDisabled() {
-    //     return true; // Solo para desarrollo, en producción configurar CORS adecuadamente
-    // }
-}
+//     // @Override
+//     // protected boolean sameOriginDisabled() {
+//     //     return true; // Solo para desarrollo, en producción configurar CORS adecuadamente
+//     // }
+// }
