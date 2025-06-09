b8916c1ff25273a1f5711c5a633328f2e290b29f
Ibai <iibaiizj@gmail.com>
2025-06-09 14:53:10 +0200

    edit security conf

--- src/main/java/com/athletix/config/SecurityConfig.java
+++ src/main/java/com/athletix/config/SecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import com.athletix.custom.CustomAuthenticationSuccessHandler;
+import com.athletix.controller.custom.CustomAuthenticationSuccessHandler;
 import com.athletix.service.custom.CustomUserDetailsService;
 
 @Configuration
