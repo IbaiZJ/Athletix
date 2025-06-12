1c227072c3e44f93a48f4982d698763a95152961
Alion983 <122363079+Alion983@users.noreply.github.com>
2025-06-12 13:40:16 +0200

    omega

--- src/main/java/com/athletix/repository/UserRepository.java
+++ src/main/java/com/athletix/repository/UserRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.athletix.model.Users;
-import java.util.List;
 
 
 public interface UserRepository extends JpaRepository<Users, Integer> {
